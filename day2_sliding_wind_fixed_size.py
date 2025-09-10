6d56b1532a7dc325858e978a411f707816b3cb89
Sai Hanuma Reddy Yarram <saihanumareddyyarram@gmail.com>
2025-09-10 21:08:18 +0530

    Update day2_sliding_wind_fixed_size.py

--- day2_sliding_wind_fixed_size.py
+++ day2_sliding_wind_fixed_size.py
@@ -18,3 +18,23 @@ def min_sum_subarray(nums, k):
     min_sum = min(min_sum, current_sum)
   return min_sum
 print(min_sum_subarray([1,4,5,6,7,8,3,1,2,5,8,4,2], 5))
+
+
+# Find all subarrays of size k in a list
+def find_subarrays(nums, k):
+  l = []
+  window = nums[:k]
+  l.append(tuple(window))
+  for i in range(k, len(nums)):
+    window = window[1:] + [nums[i]]
+    l.append(tuple(window))
+  return l
+
+print(find_subarrays([x for x in range(11)], 5))
+
+# Return their sums in a list
+def find_sums(l):
+  l_sum = list(map(lambda x: sum(x), l))
+  return l_sum
+
+print(find_sums(find_subarrays([x for x in range(11)], 5)))
